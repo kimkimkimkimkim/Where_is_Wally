1d7c5652f0caabef4f1c05943331db17702ed29b
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2018-12-19 18:43:52 +0900

    【機能追加】風船追加

---
+++ Where_is_Wally/Assets/Script/BalloonMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonMove : MonoBehaviour {
+
+	private float posY;
+	private float dist = 2;
+	private float timeanim = 1.5f;
+	// Use this for initialization
+	void Start () {
+		posY = this.transform.position.y;
+		MoveUp();
+	}
+	
+	private void MoveUp(){
+		iTween.MoveTo(gameObject, iTween.Hash("y",posY + dist,"time",timeanim
+			,"oncomplete","MoveDown","oncompletetarget",gameObject,"easetype",iTween.EaseType.easeInOutSine));
+	}
+
+	private void MoveDown(){
+		iTween.MoveTo(gameObject, iTween.Hash("y",posY - dist,"time",timeanim
+			,"oncomplete","MoveUp","oncompletetarget",gameObject,"easetype",iTween.EaseType.easeInOutSine));
+	}
+
+
+
+}
